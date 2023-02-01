1efb559a8b612150296abd0e4832dfd7c8dfaacb
Viet Nguyen <viet.nguyen@oivan.com>
2023-02-01 17:40:36 +0700

    update kong

---
+++ marketplaces-tools/config/konga/node.js
@@ -0,0 +1,10 @@
+// IMPORTANT: https://github.com/pantsel/konga/blob/master/docs/SEED_DEFAULT_DATA.md
+
+module.exports = [
+  {
+    "name": "Kong Node 1",
+    "type": "default",
+    "kong_admin_url": "http://kong:8001",
+    "health_checks": true,
+  }
+]
---
+++ marketplaces-tools/config/konga/user.js
@@ -0,0 +1,14 @@
+// IMPORTANT: https://github.com/pantsel/konga/blob/master/docs/SEED_DEFAULT_DATA.md
+
+module.exports = [
+  {
+    "username": "admin",
+    "email": "admin@admin.domain",
+    "firstName": "admin",
+    "lastName": "admin",
+    "node_id": "http://kong:8001",
+    "admin": true,
+    "active": true,
+    "password": "notadmin"
+  }
+]
